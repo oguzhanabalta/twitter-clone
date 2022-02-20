4b79a68e7307e34a4b69da440493633a67ccf516
Oguzhan <oguzhanbalta99@gmail.com>
2022-02-20 12:14:48 +0300

    SidebarLink component added.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -10,22 +10,30 @@ import {
     DotsCircleHorizontalIcon,
     DotsHorizontalIcon,
 } from "@heroicons/react/outline";
+import SidebarLink from "./SidebarLink";
 
 function Sidebar() {
 
     return (
         <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
-            <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+            <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-16">
                 <Image src="https://rb.gy/ogau5a" width={30} height={30} />
             </div>
-            <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
-               ss
+            <div className="space-y-2.5 mt-2 mb-2 xl:ml-16">
+                <SidebarLink text="Home" Icon={HomeIcon} active />
+                <SidebarLink text="Explore" Icon={HashtagIcon} />
+                <SidebarLink text="Notifications" Icon={BellIcon} />
+                <SidebarLink text="Messages" Icon={InboxIcon} />
+                <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+                <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+                <SidebarLink text="Profile" Icon={UserIcon} />
+                <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
             </div>
-            <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
+            <button className="hidden xl:inline ml-16 bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
                 Tweet
             </button>
             <div
-                className="text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-auto xl:-mr-5"
+                className="text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-16 xl:-mr-5"
             >
                 <img
                     src="https://pbs.twimg.com/profile_images/1361059521611911168/3fIO8z-w_400x400.jpg"
