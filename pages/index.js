702efb4e455774790810b035a51875c748dc14a0
Oguzhan <oguzhanbalta99@gmail.com>
2022-02-23 22:15:43 +0300

    change data

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export async function getServerSideProps(context) {
   const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
     (res) => res.json()
   );
-  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+  const followResults = await fetch("https://jsonkeeper.com/b/86FR").then(
     (res) => res.json()
   );
   const providers = await getProviders();
